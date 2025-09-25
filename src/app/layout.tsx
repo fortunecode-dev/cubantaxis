5bdf917b21731a894c6e03eed7ac7b62b8ac5db7
fortunecode-dev <fortunecode-dev@mail.com>
2025-09-25 08:25:24 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import FloatingContacts from "@/modules/layout/FloatingContacts";
 import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { LocaleParams } from "@/types/common";
 export async function generateStaticParams() {
   const languages = ["en", "fr", "de", "ru", "pt"];
   return languages.map((lang) => ({ lang }));
@@ -14,14 +13,11 @@ export async function generateStaticParams() {
 
 export default async function RootLayout({
   children,
-  params
 }: Readonly<{
   children: React.ReactNode;
-  params: LocaleParams
 }>) {
-  const { lang } = await params;
   return (
-    <html lang={lang||"en"}>
+    <html lang={"en"}>
       <head>
         {/* preconnect para handshake más rápido */}
         <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="" />
